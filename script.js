b01130b17a6e8687867169a2e092f41f1cb902d3
Matei Leonard <leonard.matei2011@yahoo.com>
2022-06-13 18:15:04 +0300

    make portofolio page and handle language change

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ function openLanguageDropDown(){
     languages.classList.add('hidden')
 
 }
-function changeLanguage(lang){
+function changeLanguageMainPage(lang){
     const language={
         'en':{
             appDescription: 'Links and info',
             altPhoto: 'photo of me',
             aboutMe: 'About me',
             myDescription: 'FrontEnd developer at SF App Works from Oct 2021. I am trying my best to become a FullStack developer.',
+            portofolio: 'Click here to see my projects!',
+            portofolioLink: 'See my portofolio',
             linkedinLink: 'See my LinkedIn profile',
             githubLink: 'See my GitHub profile',
             email: 'Send me an e-mail!'
@@ -21,11 +23,14 @@ function changeLanguage(lang){
             altPhoto: 'O poză cu mine',
             aboutMe: 'Despre mine',
             myDescription: 'FrontEnd developer la SF App Works din octombrie 2021. Îmi doresc să ajung pe o poziție de FullStack developer.',
+            portofolio: 'Aici îmi poți vedea portofoliul!',
+            portofolioLink: 'Intră pe portofoliul meu de proiecte',
             linkedinLink: 'Intră pe profilul meu de LinkedIn',
             githubLink: 'Intră pe profilul meu de GitHub',
             email: 'Trimite-mi un mail!'
         }
     }
+
     document.querySelector('meta[name="description"]').setAttribute('content',language[lang].appDescription)
     document.querySelector('meta[property="og:description"]').setAttribute('content',language[lang].appDescription)
     document.querySelector('meta[property="article:section"]').setAttribute('content',language[lang].appDescription)
@@ -35,12 +40,21 @@ function changeLanguage(lang){
     document.querySelector('.DescrierePrincipala').textContent = language[lang].appDescription
     document.querySelector('.ListaDescriereTitlu span:nth-child(2)').textContent = language[lang].aboutMe
     document.querySelector('.ListaDescriereText').textContent = language[lang].myDescription
+    document.querySelector('#portofolio .ListaText').textContent = language[lang].portofolio
+    document.getElementById('portofolio').setAttribute('title', language[lang].linkedinLink)
     document.getElementById('linkedinLink').setAttribute('title', language[lang].linkedinLink)
     document.getElementById('githubLink').setAttribute('title', language[lang].githubLink)
     document.getElementById('email').setAttribute('title', language[lang].email)
 }
 function selectLanguage(language){
+    document.documentElement.setAttribute("lang", language==='ro'?'ro-RO':'en-US');
     window.localStorage.setItem('language',language)
     document.querySelector('.LanguageButton .language-icon').setAttribute('src',language==='ro'?'./assets/ro.svg':'./assets/gb.svg')
-    changeLanguage(language)
+   
+
+    if(window.location.href.toString().split(window.location.host)[1] === '/'){
+        changeLanguageMainPage(language)
+    }
 }
+
+selectLanguage(window.localStorage.getItem('language'))
